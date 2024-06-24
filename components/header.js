5ac29f9e24b88df6a0a32028e4998dfe85396b2c
Ewen_lp <stagiaire-1@3codes.io>
2024-06-24 15:55:07 +0200

    Update header.js

--- components/header.js
+++ components/header.js
@@ -3,7 +3,7 @@ export default function Header() {
       <header className="container flex justify-between bg-lime-700">
 
         <img src="/logo-ac.png" alt="Animal Crossing Logo 2" className="object-contain h-20 w-20" />
-        <img src="/ac.png" alt="Animal Crossing logo" className="object-contain h-100 w-100" />
+        <img src="/ac.png" alt="Animal Crossing logo" className="object-contain h-60 w-100" />
 
         <div className="flex justify-center">
           <a className="text-sm" href="">Accueil</a>
