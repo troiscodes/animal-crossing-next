de2667739c1460083e360e9f39dcff7a7919adfa
Ewen_lp <stagiaire-1@3codes.io>
2024-06-24 14:59:00 +0200

    image-centrée-header

--- components/header.js
+++ components/header.js
@@ -1,9 +1,13 @@
-
 export default function Header() {
     return (
-      <header className="container">
-        <img src="/logo-ac.jpeg" alt="Animal Crossing Logo 2" class="object-contain h-20 w-20"/>
-        <img src="/ac.png" alt="Animal Crossing logo" class="object-contain h-20 w-20"/>
+      <header className="container flex justify-between bg-lime-700">
+
+            <img src="/logo-ac.png" alt="Animal Crossing Logo 2" className="object-contain h-20 w-20"/>
+            <img src="/ac.png" alt="Animal Crossing logo" className="object-contain h-20 w-100"/>
+            
+        <div className="flex items-center justify-center">
+            <p className="text-sm" href="./app/page.js"> Accueil Personnages Jeux </p>
+        </div>
       </header>
     )
-  }
+}
