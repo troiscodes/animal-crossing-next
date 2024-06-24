6bf373205d3a1906c2c4bb03050fd9aac5673d19
Ewen_lp <stagiaire-1@3codes.io>
2024-06-24 15:44:12 +0200

    header image + liens

--- components/header.js
+++ components/header.js
@@ -2,12 +2,16 @@ export default function Header() {
     return (
       <header className="container flex justify-between bg-lime-700">
 
-            <img src="/logo-ac.png" alt="Animal Crossing Logo 2" className="object-contain h-20 w-20"/>
-            <img src="/ac.png" alt="Animal Crossing logo" className="object-contain h-20 w-100"/>
-            
-        <div className="flex items-center justify-center">
-            <p className="text-sm" href="./app/page.js"> Accueil Personnages Jeux </p>
+        <img src="/logo-ac.png" alt="Animal Crossing Logo 2" className="object-contain h-20 w-20" />
+        <img src="/ac.png" alt="Animal Crossing logo" className="object-contain h-100 w-100" />
+
+        <div className="flex justify-center">
+          <a className="text-sm" href="">Accueil</a>
+          <span className="mx-1" />
+          <a className="text-sm" href="">Personnages</a>
+          <span className="mx-1" />
+          <a className="text-sm" href="">Jeux</a>
         </div>
       </header>
-    )
-}
+    );
+  }
