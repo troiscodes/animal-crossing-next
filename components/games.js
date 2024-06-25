e2bd6fc0af0e04339fdf928df6c133b32da05241
Ewen_lp <stagiaire-1@3codes.io>
2024-06-25 17:32:47 +0200

    games grid

--- components/games.js
+++ components/games.js
@@ -1,10 +1,24 @@
 export default function Games(){
     return (
-        <section>
-             <div
-      className="bg-cover bg-center min-h-[1036.87px] w-full flex items-center justify-center"
-      style={{ backgroundImage: "url('/background_section_games.png')" }}>
-            <h1 className="container">Games</h1>
+        <section style={{ backgroundImage: "url('/background_section_games.png')" }}>
+             <div className="grid grid-cols-3 place-items-center bg-cover bg-center min-h-[1036.87px]" >
+             <div>
+                    jeu 1
+                </div>
+                <div>
+                    jeu 2
+                </div>
+                <div>
+                    jeu 3
+                </div>
+            </div>
+            <div className="grid grid-cols-2 place-items-center">
+                <div>
+                    jeu 1
+                </div>
+                <div>
+                    jeu 1
+                </div>
             </div>
         </section>
     )
