acceb908104c4f5bb4111f24d0c6e3f5a454bfc8
Startine17-Ange <startine17@gmail.com>
2024-06-25 14:14:31 +0200

    footer fini

--- components/footer.js
+++ components/footer.js
@@ -2,12 +2,11 @@ export default function Footer(){
 
     return (
         <footer>
-            <div className="div_footer !bg-purple-800">
-            <div className="animal_crossing-footer"><h1 className="text_footer">Animal Crossing</h1></div>
-            <div className="footer">
-                <img className="logo-ac" src="logo-ac.png" alt="logo Animal Crossing feuille"/>
-            <h2 className="text_footer_2">créé par Clément SIMON et Ange TARDY</h2>
-            </div>
+            <div className="w-full h-2 bg-[#3FA367]"></div>
+            <div className="flex bg-[#417C12] h-[200px] w-full p-[10px] items-center justify-between">
+                <div className="w-[308px]"><img className="w-[160px] h-[160px] flex justify-self-start" src="logo-ac.png" alt="logo Animal Crossing feuille"/></div>
+                <div className="w-auto"><h1 className="text-white text-[40px]">Animal Crossing</h1></div>
+            <h2 className="text-white flex self-end justify-self-end w-[308px]">créé par Clément SIMON et Ange TARDY</h2>
             </div>
         </footer>
     )
--- components/hero.js
+++ components/hero.js
@@ -6,8 +6,8 @@ export default function Hero(){
         <section>
             <div>
             <div className="container">
-                <p className="hero">Primi igitur omnium statuuntur Epigonus et Eusebius ob nominum gentilitatem oppressi. praediximus enim Montium sub ipso vivendi termino his vocabulis appellatos fabricarum culpasse tribunos ut adminicula futurae molitioni pollicitos.</p></div>
-                <img className="img_hero" src="hero.jpg" alt="Hero"/>
+                <p className="text-center absolute w-[45%] top-[60%] left-[70%] translate-x-[-50%] translate-y-[-50%]">Primi igitur omnium statuuntur Epigonus et Eusebius ob nominum gentilitatem oppressi. praediximus enim Montium sub ipso vivendi termino his vocabulis appellatos fabricarum culpasse tribunos ut adminicula futurae molitioni pollicitos.</p></div>
+                <img className="w-full" src="hero.jpg" alt="Hero"/>
             </div>
         </section>
     )
