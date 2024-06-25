797ca57e3454724281fc4eccebe224d346925dc2
Ewen_lp <stagiaire-1@3codes.io>
2024-06-25 16:31:41 +0200

    header enfin fini après 8h

--- components/header.js
+++ components/header.js
@@ -3,18 +3,21 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="container flex justify-between bg-lime-700">
-  <div className="h-[400px] border border-red-500 w-full relative">
-          <Image src="/logo-ac.png" alt="Animal Crossing Logo 2" className="image-header-1" fill/>
-  </div>
-          <img src="/ac.png" alt="Animal Crossing logo" className="image-header-2 " />
-  
-          <div className="text-red-500 " >
-            <a href="">Accueil</a>
-            <a href="">Personnages</a>
-            <a href="">Jeux</a>
-            <Link href="/" target="_blank"><span className="text-blue-500">Acc</span>ueil</Link>
-          </div>
-        </header>
-    );
-  }
+    <header>
+      <section className="bg-lime-700 flex justify-between relative h-40">
+        <div>
+          <Image src="/logo-ac.png" alt="Animal Crossing Logo 2" width={150} height={100} className="object-scale-down absolute items-center h-30" />
+        </div>
+        <div className="text-white flex space-x-4 items-center text-xl pr-9">
+          <Link href="/">Accueil</Link>
+          <Link href="/personnages">Personnages</Link>
+          <Link href="/jeux">Jeux</Link>
+        </div>
+      </section>
+      <div className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 top-28 ">
+        <Image src="/ac.png" alt="Animal Crossing logo" width={300} height={10}/>
+      </div>
+      <div class="border-4 border-[#16a34a] "></div>
+    </header>
+  );
+}
