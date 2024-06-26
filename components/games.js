487b2532cf033892dfeb24e0b45e6e92a87e260d
Ewen_lp <stagiaire-1@3codes.io>
2024-06-26 10:01:16 +0200

    main games images

--- components/games.js
+++ components/games.js
@@ -1,23 +1,25 @@
+import Image from "next/image";
+import Link from "next/link"
+
 export default function Games(){
     return (
-        <section style={{ backgroundImage: "url('/background_section_games.png')" }}>
-             <div className="grid grid-cols-3 place-items-center bg-cover bg-center min-h-[1036.87px]" >
-             <div>
-                    jeu 1
+        <section style={{ backgroundImage: "url('/background_section_games.png')" }} className="min-h-[1036.87px]">
+            <h1 className="flex justify-center bg-white">Jeux principaux</h1>
+             <div className="grid grid-cols-3 place-items-center" >
+                <div>
+                    <Image src="/animal_crossing.png" width={350} height={100}/>
                 </div>
                 <div>
-                    jeu 2
+                    <Image src="/animal-crossing-city-folk.png" width={350} height={100}/>
                 </div>
                 <div>
-                    jeu 3
-                </div>
-            </div>
-            <div className="grid grid-cols-2 place-items-center">
-                <div>
-                    jeu 1
+                    <Image src="/animal-crossing-wild-world.png" width={350} height={100}/>
                 </div>
                 <div>
-                    jeu 1
+                    <Image src="/animal_crossing_new_leaf.png" width={350} height={100} />
+                </div>
+                <div>
+                    <Image src="/animal_crossing_new_horizons.png" width={350} height={100}/>
                 </div>
             </div>
         </section>
