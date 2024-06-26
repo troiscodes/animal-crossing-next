e46f5e97ed862099aa33ec0a767a121a691db126
Ewen_lp <stagiaire-1@3codes.io>
2024-06-26 11:12:06 +0200

    main games fini

--- components/games.js
+++ components/games.js
@@ -3,23 +3,40 @@ import Link from "next/link"
 
 export default function Games(){
     return (
-        <section style={{ backgroundImage: "url('/background_section_games.png')" }} className="min-h-[1036.87px]">
-            <h1 className="flex justify-center bg-white">Jeux principaux</h1>
-             <div className="grid grid-cols-3 place-items-center" >
+        <section style={{ backgroundImage: "url('/background_section_games.png') " }} className="min-h-[1036.87px] relative">
+                <div className="h-[114.51px] w-full flex justify-center pt-14">
+                <div className="h-[114.51px] w-[335.75px] bg-[#F5FFE4] drop-shadow-xl -rotate-3">
+                    <h1 className="flex justify-center items-center h-[114px] w-[335] text-3xl">Jeux principaux</h1>
+                    </div>
+                </div>
+
+            <div className="grid grid-cols-3 place-items-center pt-24 pr-20 pl-20">
                 <div>
+                <a href="/jeux">
                     <Image src="/animal_crossing.png" width={350} height={100}/>
+                    </a>
                 </div>
                 <div>
+                <a href="/jeux">
                     <Image src="/animal-crossing-city-folk.png" width={350} height={100}/>
+                    </a>
                 </div>
                 <div>
+                <a href="/jeux">
                     <Image src="/animal-crossing-wild-world.png" width={350} height={100}/>
+                    </a>
                 </div>
-                <div>
-                    <Image src="/animal_crossing_new_leaf.png" width={350} height={100} />
-                </div>
-                <div>
-                    <Image src="/animal_crossing_new_horizons.png" width={350} height={100}/>
+                <div className="col-span-3 flex justify-between gap-24 pt-8">
+                    <div>
+                        <a href="/jeux">
+                        <Image src="/animal_crossing_new_leaf.png" width={350} height={100}/>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="/jeux">
+                        <Image src="/animal_crossing_new_horizons.png" width={350} height={100} />
+                        </a>
+                    </div>
                 </div>
             </div>
         </section>
