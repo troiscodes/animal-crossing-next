6d3aad604ee4763097d0fbb4dba82560fafb3bd1
Ewen_lp <stagiaire-1@3codes.io>
2024-06-26 11:57:55 +0200

    header hover

--- components/header.js
+++ components/header.js
@@ -8,10 +8,10 @@ export default function Header() {
         <div>
           <Image src="/logo-ac.png" alt="Animal Crossing Logo 2" width={150} height={100} className="object-scale-down absolute items-center h-30" />
         </div>
-        <div className="text-white flex space-x-4 items-center text-xl pr-9 font-fink-heavy">
-          <Link href="/">Accueil</Link>
-          <Link href="/personnages">Personnages</Link>
-          <Link href="/jeux">Jeux</Link>
+        <div className="text-white flex space-x-4 items-center text-xl pr-9 font-fink-heavy ">
+          <Link href="/" className="hover:underline">Accueil</Link>
+          <Link href="/personnages" className="hover:underline" >Personnages</Link>
+          <Link href="/jeux" className="hover:underline">Jeux</Link>
         </div>
       </section>
       <div className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 top-28 ">
