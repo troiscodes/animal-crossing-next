71973ae51f42d880fdde5530ae92b5709dc83e62
Ewen_lp <stagiaire-1@3codes.io>
2024-06-27 12:35:17 +0200

    main games responsive fini

--- app/jeux/page.js
+++ app/jeux/page.js
@@ -10,13 +10,13 @@ export default function Home(){
             <section className="flex items-center md:pt-8">
                 <div className="md:flex items-center">
                 <div><img src="/jeu-gamecube.png" className="pt-12 pl-7 pb-6 container flex justify-center" ></img></div>
-                <div className="relative text-2xl ml-6 mr-6 pl-8 pr-8 flex md:w-full md:min-h-[357px] rounded-3xl" style={{ backgroundColor: 'rgba(111, 238, 40, 0.3)' }}>
+                <div className="relative text-2xl ml-6 mr-6 pt-4 pl-6 pr-6 flex md:w-full md:min-h-[357px] rounded-3xl" style={{ backgroundColor: 'rgba(111, 238, 40, 0.3)' }}>
                     <div className="grid md:grid-cols-7">
-                        <div className="relative md:col-span-5 flex items-center">
+                        <div className="relative md:col-span-5 flex items-center pb-4">
                             Dans ce jeu, le joueur emménage dans un village de campagne habité par des animaux aux caractères bien distincts. Il introduit un nouveau type de jeu dérivé du collect them up : le but est de rendre tous les habitants heureux, de créer des vêtements et des décorations et de collectionner des objets (meubles, tapis, vêtements, fossiles…). Le jeu se déroule en temps réel, les minutes et les heures étant synchronisées avec la vie réelle (heure de la console), ainsi que les jours, les mois et les saisons. Il est également possible de fêter Noël et tous les autres événements avec les villageois.
                         </div>
                         <div className=" md:col-span-1">
-                            <Image src="/jeu-gamecube-image.png" width={300} height={100} className="md:absolute bottom-0 right-0" />
+                            <Image src="/jeu-gamecube-image.png" width={350} height={100} className="md:absolute bottom-0 right-0" />
                         </div>
                     </div>
                 </div>
@@ -27,13 +27,13 @@ export default function Home(){
             <section className="flex items-center md:pt-8">
                 <div className="md:flex items-center">
                 <div><img src="/jeu-ds.png" className="pt-12 pl-7 pb-6 container flex justify-center" ></img></div>
-                <div className="relative text-2xl ml-6 mr-6 pl-8 pr-8 flex md:w-full md:min-h-[357px] rounded-3xl" style={{ backgroundColor: 'rgba(111, 238, 40, 0.3)' }}>
+                <div className="relative text-2xl ml-6 mr-6 pt-4 pl-6 pr-6 flex md:w-full md:min-h-[357px] rounded-3xl" style={{ backgroundColor: 'rgba(111, 238, 40, 0.3)' }}>
                     <div className="grid md:grid-cols-7">
-                        <div className="relative md:col-span-5 flex items-center">
+                        <div className="relative md:col-span-5 flex items-center pb-4">
                             Dans ce jeu, le joueur emménage dans un village de campagne habité par des animaux aux caractères bien distincts. Il introduit un nouveau type de jeu dérivé du collect them up : le but est de rendre tous les habitants heureux, de créer des vêtements et des décorations et de collectionner des objets (meubles, tapis, vêtements, fossiles…). Le jeu se déroule en temps réel, les minutes et les heures étant synchronisées avec la vie réelle (heure de la console), ainsi que les jours, les mois et les saisons. Il est également possible de fêter Noël et tous les autres événements avec les villageois.
                         </div>
                         <div className=" md:col-span-1">
-                            <Image src="/jeu-ds-image.png" width={300} height={100} className="md:absolute bottom-0 right-0" />
+                            <Image src="/jeu-ds-image.png" width={350} height={100} className="md:absolute bottom-0 right-0" />
                         </div>
                     </div>
                 </div>
@@ -43,13 +43,13 @@ export default function Home(){
             <section className="flex items-center md:pt-8">
                 <div className="md:flex items-center">
                 <div><img src="/jeu-wii.png" className="pt-12 pl-7 pb-6 container flex justify-center" ></img></div>
-                <div className="relative text-2xl ml-6 mr-6 pl-8 pr-8 flex md:w-full md:min-h-[357px] rounded-3xl" style={{ backgroundColor: 'rgba(111, 238, 40, 0.3)' }}>
+                <div className="relative text-2xl ml-6 mr-6 pt-4 pl-6 pr-6 flex md:w-full md:min-h-[357px] rounded-3xl" style={{ backgroundColor: 'rgba(111, 238, 40, 0.3)' }}>
                     <div className="grid md:grid-cols-7">
-                        <div className="relative md:col-span-5 flex items-center">
+                        <div className="relative md:col-span-5 flex items-center pb-4">
                             Dans ce jeu, le joueur emménage dans un village de campagne habité par des animaux aux caractères bien distincts. Il introduit un nouveau type de jeu dérivé du collect them up : le but est de rendre tous les habitants heureux, de créer des vêtements et des décorations et de collectionner des objets (meubles, tapis, vêtements, fossiles…). Le jeu se déroule en temps réel, les minutes et les heures étant synchronisées avec la vie réelle (heure de la console), ainsi que les jours, les mois et les saisons. Il est également possible de fêter Noël et tous les autres événements avec les villageois.
                         </div>
                         <div className=" md:col-span-1">
-                            <Image src="/jeu-wii-image.png" width={300} height={100} className="md:absolute bottom-0 right-0" />
+                            <Image src="/jeu-wii-image.png" width={350} height={100} className="md:absolute bottom-0 right-0" />
                         </div>
                     </div>
                 </div>
@@ -59,13 +59,13 @@ export default function Home(){
             <section className="flex items-center md:pt-8">
                 <div className="md:flex items-center">
                 <div><img src="/jeu-3ds.png" className="pt-12 pl-7 pb-6 container flex justify-center" ></img></div>
-                <div className="relative text-2xl ml-6 mr-6 pl-8 pr-8 flex md:w-full md:min-h-[357px] rounded-3xl" style={{ backgroundColor: 'rgba(111, 238, 40, 0.3)' }}>
+                <div className="relative text-2xl ml-6 mr-6 pt-4 pl-6 pr-6 flex md:w-full md:min-h-[357px] rounded-3xl" style={{ backgroundColor: 'rgba(111, 238, 40, 0.3)' }}>
                     <div className="grid md:grid-cols-7">
-                        <div className="relative md:col-span-5 flex items-center">
+                        <div className="relative md:col-span-5 flex items-center pb-4">
                             Dans ce jeu, le joueur emménage dans un village de campagne habité par des animaux aux caractères bien distincts. Il introduit un nouveau type de jeu dérivé du collect them up : le but est de rendre tous les habitants heureux, de créer des vêtements et des décorations et de collectionner des objets (meubles, tapis, vêtements, fossiles…). Le jeu se déroule en temps réel, les minutes et les heures étant synchronisées avec la vie réelle (heure de la console), ainsi que les jours, les mois et les saisons. Il est également possible de fêter Noël et tous les autres événements avec les villageois.
                         </div>
                         <div className=" md:col-span-1">
-                            <Image src="/jeu-3ds-image.png" width={300} height={100} className="md:absolute bottom-0 right-0" />
+                            <Image src="/jeu-3ds-image.png" width={350} height={100} className="md:absolute bottom-0 right-0" />
                         </div>
                     </div>
                 </div>
@@ -75,13 +75,13 @@ export default function Home(){
             <section className="flex items-center md:pt-8">
                 <div className="md:flex items-center mb-5" >
                 <div><img src="/jeu-switch.png" className="pt-12 pl-7 pb-6 container flex justify-center" ></img></div>
-                <div className="relative text-2xl ml-6 mr-6 pl-8 pr-8 flex md:w-full md:min-h-[357px] rounded-3xl" style={{ backgroundColor: 'rgba(111, 238, 40, 0.3)' }}>
+                <div className="relative text-2xl ml-6 mr-6 pt-4 pl-6 pr-6 flex md:w-full md:min-h-[357px] rounded-3xl" style={{ backgroundColor: 'rgba(111, 238, 40, 0.3)' }}>
                     <div className="grid md:grid-cols-7">
-                        <div className="relative md:col-span-5 flex items-center">
+                        <div className="relative md:col-span-5 flex items-center pb-4">
                             Dans ce jeu, le joueur emménage dans un village de campagne habité par des animaux aux caractères bien distincts. Il introduit un nouveau type de jeu dérivé du collect them up : le but est de rendre tous les habitants heureux, de créer des vêtements et des décorations et de collectionner des objets (meubles, tapis, vêtements, fossiles…). Le jeu se déroule en temps réel, les minutes et les heures étant synchronisées avec la vie réelle (heure de la console), ainsi que les jours, les mois et les saisons. Il est également possible de fêter Noël et tous les autres événements avec les villageois.
                         </div>
                         <div className=" md:col-span-1">
-                            <Image src="/jeu-switch-image.png" width={300} height={100} className="md:absolute bottom-0 right-0" />
+                            <Image src="/jeu-switch-image.png" width={350} height={100} className="md:absolute bottom-0 right-0" />
                         </div>
                     </div>
                 </div>
