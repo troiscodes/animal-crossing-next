8b707abfbd307afe0633af00f00a31c489c126fc
Startine17-Ange <startine17@gmail.com>
2024-06-27 12:11:33 +0200

    fix grid main games and responsive main games

--- components/games.js
+++ components/games.js
@@ -3,42 +3,43 @@ import Link from "next/link"
 
 export default function Games(){
     return (
-        <section style={{ backgroundImage: "url('/background_section_games.png') " }} className="min-h-[1036.87px] relative">
-                <div className="h-[114.51px] w-full flex justify-center pt-14">
-                <div className="h-[114.51px] w-[335.75px] bg-[#F5FFE4] drop-shadow-xl -rotate-3">
-                    <h1 className="flex justify-center items-center h-[114px] w-[335] text-3xl font-fink-heavy">Jeux principaux</h1>
+        <section style={{ backgroundImage: "url('/background_section_games.png') " }} className="md:min-h-[1036.87px] min-h-[1900px]">
+                <div className="h-[114.51px] w-full flex flex-col justify-start items-center pt-14">
+                <div className="md:h-[114.51px] md:w-[398.75px] h-[100px] w-[330px] bg-[#F5FFE4] drop-shadow-xl -rotate-3">
+                    <h1 className="flex justify-center items-center md:h-[114px] md:w-[398px] h-[100px] w-[330px] text-3xl font-fink-heavy">Jeux principaux</h1>
                     </div>
+
+            <div className="container w-full grid md:grid-cols-3 grid-cols-1 place-items-center pt-14 px-10 md:mt-10 md:gap-14 gap-10 md:m-5">
+                <div>
+                <a href="/jeux">
+                    <img src="/animal_crossing.png" className="transition-transform ease-in-out hover:scale-110 rounded-full md:h-[350px] md:w-[350px] h-[300px] w-[300px]"/>
+                    </a>
+                </div>
+                <div>
+                <a href="/jeux">
+                    <img src="/animal-crossing-city-folk.png" className="transition-transform ease-in-out hover:scale-110 rounded-full md:h-[350px] md:w-[350px] h-[300px] w-[300px]"/>
+                    </a>
+                </div>
+                <div>
+                <a href="/jeux">
+                    <img src="/animal-crossing-wild-world.png" className="transition-transform ease-in-out hover:scale-110 rounded-full md:h-[350px] md:w-[350px] h-[300px] w-[300px]"/>
+                    </a>
+                </div>
                 </div>
 
-            <div className="grid grid-cols-3 place-items-center pt-24 pr-20 pl-20">
-                <div>
-                <a href="/jeux">
-                    <Image src="/animal_crossing.png" width={350} height={100} className="transition-transform ease-in-out hover:scale-110 rounded-full"/>
-                    </a>
-                </div>
-                <div>
-                <a href="/jeux">
-                    <Image src="/animal-crossing-city-folk.png" width={350} height={100} className="transition-transform ease-in-out hover:scale-110 rounded-full"/>
-                    </a>
-                </div>
-                <div>
-                <a href="/jeux">
-                    <Image src="/animal-crossing-wild-world.png" width={350} height={100} className="transition-transform ease-in-out hover:scale-110 rounded-full"/>
-                    </a>
-                </div>
-                <div className="col-span-3 flex justify-between gap-24 pt-8">
+                <div className="container place-items-center grid md:grid-cols-2 grid-cols-1 w-full md:px-48 pt-10 px-10 md:gap-5 gap-10 md:m-10">
                     <div>
                         <a href="/jeux">
-                        <Image src="/animal_crossing_new_leaf.png" width={350} height={100} className="transition-transform ease-in-out hover:scale-110 rounded-full"/>
+                        <img src="/animal_crossing_new_leaf.png" className="transition-transform ease-in-out hover:scale-110 rounded-full md:h-[350px] md:w-[350px] h-[300px] w-[300px]"/>
                         </a>
                     </div>
                     <div>
                         <a href="/jeux">
-                        <Image src="/animal_crossing_new_horizons.png" width={350} height={100} className="transition-transform ease-in-out hover:scale-110 rounded-full"/>
+                        <img src="/animal_crossing_new_horizons.png" className="transition-transform ease-in-out hover:scale-110 rounded-full md:h-[350px] md:w-[350px] h-[300px] w-[300px]"/>
                         </a>
+                        </div>
                     </div>
-                </div>
-            </div>
+                    </div>
         </section>
     )
 }
