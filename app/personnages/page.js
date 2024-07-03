cbb23fa9237059c3964594827bc60a195638e883
Startine17-Ange <startine17@gmail.com>
2024-07-03 17:23:06 +0200

    essai

--- app/personnages/page.js
+++ app/personnages/page.js
@@ -1,79 +1,115 @@
-// import CharacterSection from "@/components/character-section";
+import CharacterSection from "/components/character-section";
 import Image from "next/image";
 
 export default async function Home() {
 
-	const response = await fetch("https://animalcrossing.dev.3codes.io/graphql", {
-		method: "POST",
-		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify({
-			query: `query {
-			characters(first: 10) {
-				edges {
-				node {
-					title
-					id
-					personnage {
-					fond {
-						node {
-						sourceUrl
-						}
-					}
-					realisateur {
-						node {
-						sourceUrl
-						}
-					}
-					}
-				}
-				}
-			}
-			}`,
-		}),
-	});
-	const { data } = await response.json();
+  const response = await fetch("https://animalcrossing.dev.3codes.io/graphql", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      query: `query {
+        characters {
+    edges {
+      node {
+      title
+        characterId
+        personnage {
+          apprendreIRL
+          description
+          fieldGroupName
+          fonction
+          cartePersonnage {
+            node {
+              sourceUrl
+              id
+            }
+          }
+          dansLeJeu {
+            node {
+              sourceUrl
+              id
+            }
+          }
+          fondH {
+            node {
+              sourceUrl
+              id
+            }
+          }
+          fondW {
+            node {
+              id
+              sourceUrl
+            }
+          }
+        }
+      }
+    }
+  }
+}`,
+    }),
+  });
+  const { data } = await response.json();
 
-	console.log(data.characters.edges, '999999999');
+  // console.log(data.characters.edges, '999999999');
 
-	const characters = data.characters.edges;
+  const characters = data.characters.edges;
+  // console.log("characters", characters);
 
-	return (
-		<main className="min-h-full w-full">
+  return (
+    <main className="min-h-full w-full flex-col-reverse">
+      <>
+        {characters && characters.map((character) => {
 
-			{characters && characters.map((character) => {
+          // console.log("character", character);
+          const backgroundWURL = character.node.personnage.fondW?.node.sourceUrl;
+          const backgroundHURL = character.node.personnage.fondH?.node.sourceUrl;
 
-				const backgroundURL = character.node.personnage.fond.node.sourceUrl;
+          console.log("backgroundWURL", backgroundWURL);
+          console.log("backgroundHURL", backgroundHURL);
+
+          return (
+            <div key={character.node.characterId}>
+              <div className="bg-cover bg-center min-h-[465px] w-full relative">
+                <Image
+                  src={backgroundWURL}
+                  alt="background"
+                  fill
+                  className="object-cover md:block hidden"
+                />
+                <Image
+                  src={backgroundHURL}
+                  alt="background_h"
+                  fill
+                  className="object-cover md:hidden block"
+                />
+
+              </div >
+            </div>
+          )
+            < div className = "py-2 px-6" > titre : {character.node.title}</div>
+      })}
 
 
-				return (
-					<>
-						<div className="relative">
-							{/* <Image src={backgroundURL} alt="background" fill className="object-cover absolute h-full" /> */}
-							<div className="py-2 px-6 bg-red-500 my-4">titre : {character.node.title}</div>
-						</div>
-					</>
-				)
-			})}
 
 
-
-			{/* <CharacterSection
-				backgroundImage={"/background_leaf.png"}
-				backgroundImageH={"/background_leaf_h.png"}
-				characterCard={"/Tom_Nook_card.png"}
-				idCard={"Tom_Nook"}
-				textD={
-					"Primi igitur omnium statuuntur Epigonus et Eusebius ob nominum gentilitatem oppressi."
-				}
-				textF={
-					"Primi igitur omnium statuuntur Epigonus et Eusebius ob nominum gentilitatem oppressi."
-				}
-				textC={
-					"Primi igitur omnium statuuntur Epigonus et Eusebius ob nominum gentilitatem oppressi."
-				}
-				Illustration={"/maison.png"}
-				backgroundColor={"bg-[#73EED4]"}
-			/>
+      {/*<CharacterSection
+        backgroundImage={{ backgroundURL }}
+        backgroundImageH={"/background_leaf_h.png"}
+        characterCard={"/Tom_Nook_card.png"}
+        idCard={"Tom_Nook"}
+        textD={
+          "Primi igitur omnium statuuntur Epigonus et Eusebius ob nominum gentilitatem oppressi."
+        }
+        textF={
+          "Primi igitur omnium statuuntur Epigonus et Eusebius ob nominum gentilitatem oppressi."
+        }
+        textC={
+          "Primi igitur omnium statuuntur Epigonus et Eusebius ob nominum gentilitatem oppressi."
+        }
+        Illustration={"/maison.png"}
+        backgroundColor={"bg-[#73EED4]"}
+      />
 			<CharacterSection
 				backgroundImage={"/background_green.png"}
 				backgroundImageH={"/background_green_h.png"}
@@ -197,7 +233,6 @@ export default async function Home() {
 				}
 				Illustration={"/bureau_maire.png"}
 				backgroundColor={"bg-[#B4447C]"}
-			/> */}
-		</main>
-	);
+			/>*/ }
+      );
 }
